00a997a54ba0a3cd5ce7e8e3728cd537d962463b
kdoerges <kdoerges@googlemail.com>
2019-02-10 22:03:44 +0100

    add keyword annotations to additional selenese keywords

--- src/main/java/org/vebqa/vebtal/selenese/command/ZAPSeleneseExtensions.java
+++ src/main/java/org/vebqa/vebtal/selenese/command/ZAPSeleneseExtensions.java
@@ -2,53 +2,15 @@ package org.vebqa.vebtal.selenese.command;
 
 import static jp.vmi.selenium.selenese.command.ArgumentType.VALUE;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map.Entry;
-import java.util.Set;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.vebqa.vebtal.GuiManager;
-import org.vebqa.vebtal.selenese.filedownloader.FileDownloader;
-import org.vebqa.vebtal.selenese.filedownloader.RequestMethod;
-import org.vebqa.vebtal.selenese.filedownloader.URLStatus;
-import org.vebqa.vebtal.seleneserestserver.SeleneseResource;
+import org.vebqa.vebtal.annotations.Keyword;
 import org.vebqa.vebtal.seleneserestserver.SeleneseTestAdaptionPlugin;
-import org.vebqa.vebtal.sut.SutStatus;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-import com.google.common.io.Files;
 
 import jp.vmi.selenium.selenese.Context;
-import jp.vmi.selenium.selenese.VarsMap;
 import jp.vmi.selenium.selenese.command.AbstractCommand;
 import jp.vmi.selenium.selenese.command.ICommand;
 import jp.vmi.selenium.selenese.command.ICommandFactory;
-import jp.vmi.selenium.selenese.result.Failure;
 import jp.vmi.selenium.selenese.result.Result;
 import jp.vmi.selenium.selenese.result.Success;
 import jp.vmi.selenium.selenese.utils.LoggerUtils;
@@ -63,9 +25,24 @@ public class ZAPSeleneseExtensions implements ICommandFactory {
 
 	private static final Logger logger = LoggerFactory.getLogger(ZAPSeleneseExtensions.class);
 
+	@Keyword(module = SeleneseTestAdaptionPlugin.ID, command = "startZap")
 	private static class StartZAP extends AbstractCommand {
 
 		StartZAP(int index, String name, String... args) {
+			super(index, name, args, VALUE, VALUE, VALUE);
+		}
+
+		@Override
+		protected Result executeImpl(Context context, String... curArgs) {
+			logger.info("Called executeImpl with Argument lengt: {}", curArgs.length);
+			return new Success("ok");
+		}
+	}
+
+	@Keyword(module = SeleneseTestAdaptionPlugin.ID, command = "stopZap")
+	private static class StopZAP extends AbstractCommand {
+
+		StopZAP(int index, String name, String... args) {
 			super(index, name, args, VALUE, VALUE, VALUE);
 		}
 
@@ -85,8 +62,11 @@ public class ZAPSeleneseExtensions implements ICommandFactory {
 	 */
 	public ICommand newCommand(int index, String name, String... args) {
 		LoggerUtils.quote("Called newCommand for " + name);
-		if (name.contentEquals("startZAP")) {
+		if (name.contentEquals("startZap")) {
 			return new StartZAP(index, name, args);
+		}
+		if (name.contentEquals("stopZap")) {
+			return new StopZAP(index, name, args);
 		}
 
 		return null;
--- src/main/java/org/vebqa/vebtal/seleneserestserver/SeleneseTestAdaptionPlugin.java
+++ src/main/java/org/vebqa/vebtal/seleneserestserver/SeleneseTestAdaptionPlugin.java
@@ -1,26 +1,49 @@
 package org.vebqa.vebtal.seleneserestserver;
 
+import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 import org.apache.commons.configuration2.CombinedConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.vebqa.vebtal.AbstractTestAdaptionPlugin;
+import org.vebqa.vebtal.CommandAutoComplete;
 import org.vebqa.vebtal.GuiManager;
+import org.vebqa.vebtal.KeywordEntry;
+import org.vebqa.vebtal.KeywordFinder;
 import org.vebqa.vebtal.TestAdaptionType;
 import org.vebqa.vebtal.model.Command;
 import org.vebqa.vebtal.model.CommandResult;
 import org.vebqa.vebtal.model.CommandType;
 import org.vebqa.vebtal.seleneserestserver.util.DriverManager;
+import org.vebqa.vebtal.sut.SutStatus;
 
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 public class SeleneseTestAdaptionPlugin extends AbstractTestAdaptionPlugin {
 
@@ -105,11 +128,59 @@ public class SeleneseTestAdaptionPlugin extends AbstractTestAdaptionPlugin {
 		
 		proxyComboBox.getSelectionModel().select(DEFAULTPROXY);
 		
+		// Add
+		// Add (Test generation)
+		Text txtGeneration = new Text();
+		txtGeneration.setText("test generation");
+		txtGeneration.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20)); 
+		
+		List<KeywordEntry> allModuleKeywords = KeywordFinder.getinstance().getKeywordsByModule(SeleneseTestAdaptionPlugin.ID);
+		TreeSet<String> sortedKeywords = new TreeSet<>();
+		for (KeywordEntry aKeyword : allModuleKeywords) {
+			sortedKeywords.add(aKeyword.getCommand());
+		}
+		
+		final CommandAutoComplete addCommand = new CommandAutoComplete(sortedKeywords);
+        addCommand.setPromptText("Command");
+        addCommand.setMaxWidth(200);
+        final TextField addTarget = new TextField();
+        addTarget.setMaxWidth(350);
+        addTarget.setPromptText("Target");
+        final TextField addValue = new TextField();
+        addValue.setMaxWidth(350);
+        addValue.setPromptText("Value");
+ 
+        final Button addButton = new Button("Go");
+        addButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+            	Command newCmd = new Command(addCommand.getText(), addTarget.getText(), addValue.getText());
+            	
+                addCommand.clear();
+                addTarget.clear();
+                addValue.clear();
+                
+                SeleneseResource aResource = new SeleneseResource();
+                GuiManager.getinstance().setTabStatus(SeleneseTestAdaptionPlugin.ID, SutStatus.CONNECTED);
+                aResource.execute(newCmd);
+                GuiManager.getinstance().setTabStatus(SeleneseTestAdaptionPlugin.ID, SutStatus.DISCONNECTED);
+            }
+        });
+ 
+        HBox hcmdbox = new HBox();
+         
+        hcmdbox.getChildren().addAll(txtGeneration, addCommand, addTarget, addValue, addButton);
+        hcmdbox.setSpacing(5);
+        hcmdbox.setBorder(new Border(new BorderStroke(Color.BLUE, BorderStrokeStyle.SOLID, new CornerRadii(2), new BorderWidths(3))));
+		
 		BorderPane pane = (BorderPane)seleneseTab.getContent();
 		// Top bauen
 		HBox hbox = new HBox();
 		hbox.getChildren().addAll(browserComboBox, proxyComboBox);
-		pane.setTop(hbox);
+		
+		VBox stack = new VBox();
+		stack.getChildren().addAll(hbox, hcmdbox);
+		pane.setTop(stack);
 		
 		return seleneseTab;
 	}
